29ac41138246af59a252629c94674f8ac6f9bb29
kiki <kiki@plgrim.com>
2024-07-03 14:36:58 +0900

    feat: nrello 프로젝트 생성
    Trello Board 형식의 어플리케이션 개발을 위한 프로젝트 생성

--- nrello/nuxt.config.ts
+++ nrello/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/ui",
+  ],
+
+  ui: {},
+
+  compatibilityDate: "2024-07-03",
 })
