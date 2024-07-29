5173946df13dd5c0cf03c01a287d0b64ad161fa8
kiki <kiki@plgrim.com>
2024-07-29 16:11:11 +0900

    feat: 보드 상세 crud api 개발

---
+++ nrello/server/api/boards/[boardId]/index.delete.ts
@@ -0,0 +1,29 @@
+import { List } from "~/server/models/List.model";
+import {Board} from "~/server/models/Board.model";
+
+export default defineEventHandler(async (event) => {
+  const boardId = getRouterParam(event, "boardId");
+  const user = event.context.user;
+
+  const deletedBoard = await Board.findOneAndDelete({
+    _id: boardId,
+    owner: user._id,
+  });
+
+  if (!deletedBoard) {
+      throw createError({
+        statusCode: 404,
+        message: "Board not found",
+      });
+  }
+
+  // 보드와 관련된 모든 목록 삭제
+  await List.deleteMany({
+    board: deletedBoard._id,
+    owner: user._id,
+  });
+
+  event.node.res.statusCode = 204;
+
+  return true;
+});
